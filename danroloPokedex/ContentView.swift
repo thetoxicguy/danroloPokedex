0abfb2a7ac9c1c8c1c1d4fca785c7c4bdafb05d8
Daniel Robles López <danrolodev@gmail.com>
2026-01-20 11:57:31 -0600

    Built FetchService and getPokemon function; online data working

--- danroloPokedex/ContentView.swift
+++ danroloPokedex/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
         animation: .default)
     private var pokedex: FetchedResults<Pokemon>
     
+    let fetcher = FetchService()
+    
     var body: some View {
         NavigationView {
             List {
@@ -33,8 +35,37 @@ struct ContentView: View {
                 }
                 ToolbarItem {
                     Button("Add Item", systemImage: "plus") {
-                        
+                        getPokemon()
                     }
+                }
+            }
+        }
+    }
+    private func getPokemon() {
+        Task {
+            for id in 1..<152 {
+                do {
+                    let fetchedPokemon = try await fetcher.fetchPokemon(id)
+                    
+//                    Set the fetchedPokemon in terms of the database storage
+                    let pokemon = Pokemon(context: viewContext)
+                    pokemon.id = fetchedPokemon.id
+                    pokemon.name = fetchedPokemon.name
+                    pokemon.types = fetchedPokemon.types
+                    pokemon.hp = fetchedPokemon.hp
+                    pokemon.attack = fetchedPokemon.attack
+                    pokemon.defense = fetchedPokemon.defense
+                    pokemon.specialAttack = fetchedPokemon.specialAttack
+                    pokemon.specialDefense = fetchedPokemon.specialDefense
+                    pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
+                    pokemon.shiny = fetchedPokemon.shiny
+                    
+//                    Save the fetched
+                    try viewContext.save()
+                    
+                } catch {
+                    print(error)
                 }
             }
         }
---
+++ danroloPokedex/FetchedPokemon.swift
@@ -0,0 +1,86 @@
+//
+//  Untitled.swift
+//  danroloPokedex
+//
+//  Created by daniel.a.robles on 20/01/26.
+//
+
+import Foundation
+
+struct FetchedPokemon: Decodable {
+    let id: Int16
+    let name: String
+    let types: [String]
+    let hp: Int16
+    let attack: Int16
+    let defense: Int16
+    let specialAttack: Int16
+    let specialDefense: Int16
+    let speed: Int16
+    let sprite: URL
+    let shiny: URL
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case name
+        case types
+        case stats
+        case sprites
+        
+        enum TypeDictionaryKeys: CodingKey {
+            case type
+            
+            enum TypeKeys: CodingKey {
+                case name
+            }
+        }
+        
+        enum StatsDictionaryKeys: CodingKey {
+            case baseStat
+        }
+        
+        enum SpriteKeys: String, CodingKey {
+            case sprite = "frontDefault"
+            case shiny = "frontShiny"
+        }
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int16.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+
+        var decodedTypes: [String] = []
+        var typesContainer = try container.nestedUnkeyedContainer(forKey: .types)
+        while !typesContainer.isAtEnd {
+            let typesDictionaryContainer = try typesContainer.nestedContainer(keyedBy: CodingKeys.TypeDictionaryKeys.self)
+            let typeContainer = try typesDictionaryContainer.nestedContainer(
+                keyedBy: CodingKeys.TypeDictionaryKeys.TypeKeys.self,
+                forKey: .type
+            )
+            let type = try typeContainer.decode(String.self, forKey: .name)
+            decodedTypes.append(type)
+            }
+        types = decodedTypes
+
+        var decodedStats: [Int16] = []
+        var statsContainer = try container.nestedUnkeyedContainer(forKey: .stats)
+        while !statsContainer.isAtEnd {
+            let statsDictionaryContainer = try statsContainer.nestedContainer(keyedBy: CodingKeys.StatsDictionaryKeys.self)
+            let stat = try statsDictionaryContainer.decode(Int16.self, forKey: .baseStat)
+            decodedStats.append(stat)
+        }
+
+        hp = decodedStats[0]
+        attack = decodedStats[1]
+        defense = decodedStats[2]
+        specialAttack = decodedStats[3]
+        specialDefense = decodedStats[4]
+        speed = decodedStats[5]
+
+        let spriteContainer =  try container.nestedContainer(keyedBy: CodingKeys.SpriteKeys.self, forKey: .sprites)
+        sprite = try spriteContainer.decode(URL.self, forKey: .sprite)
+        shiny = try spriteContainer.decode(URL.self, forKey: .shiny)
+    }
+}
